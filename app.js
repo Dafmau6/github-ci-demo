9300c381f2e5cc4de0f35a69011df3592e0af91c
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-06-21 18:29:01 -0500

    nueva version

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+   // res.send("hello world");
+   res.sendfile(path.join(__dirname + "/index.html"))
+});
+     
+
+app.listen(3000, ()=>{
+    console.log("server running on port", 3000);
+});
+
